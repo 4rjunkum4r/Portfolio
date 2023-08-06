4b071acf21071d6e87749f18540ab18df0b1ca0f
Arjun Kumar <kumararjundamai1999@gmail.com>
2023-08-06 20:31:37 +0530

    08/06/2023 Second Push

---
+++ src/Homepage.js
@@ -0,0 +1,14 @@
+import WebsiteNavbar from "./WebsiteNavbar";
+
+function Homepage(){
+    return(
+        <div>
+            <WebsiteNavbar/>
+            This is Homepage....
+            <p>
+                Hi this is paragraph
+            </p>
+        </div>
+    );
+}
+export default Homepage;
